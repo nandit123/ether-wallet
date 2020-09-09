9df173936f987fd1c7a3615f74c06d66a032d639
Nandit <nanditmehra123@gmail.com>
2020-09-09 18:02:56 +0530

    contract up and running, connected to the frontend

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,11 @@
 const Migrations = artifacts.require("Migrations");
 const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = async function (deployer) {
+module.exports = async function(deployer) {
   await deployer.deploy(Migrations);
   await deployer.deploy(DaiTokenMock);
-  const tokenMock = await DaiTokenMock.deployed();
-  // mint 1000 tokens for the deployer
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
   await tokenMock.mint(
     '0xA917694F3f033B53659d57C9dD1A32d368210B31',
     '1000000000000000000000'
