756a0da969c995412c9bd27668f4a05e07eb4e77
Nandit <nanditmehra123@gmail.com>
2020-09-09 10:07:20 +0530

    deployed initial contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed();
+  // mint 1000 tokens for the deployer
+  await tokenMock.mint(
+    '0xA917694F3f033B53659d57C9dD1A32d368210B31',
+    '1000000000000000000000'
+  )
 };
